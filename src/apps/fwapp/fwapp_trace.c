838a2fa92a98a86525c0eb7c0b101d003627fd89
Denis Shienkov <denis.shienkov@gmail.com>
2021-06-05 15:06:18 +0300

    fwapp: Use traces on USART2

--- src/apps/fwapp/fwapp_trace.c
+++ src/apps/fwapp/fwapp_trace.c
@@ -8,22 +8,24 @@
 
 #include <errno.h> // for errno
 
-#define TRACE_DATA_SIZE       512
+// We use USART2 TX for traces (pin PA2).
+
+#define TRACE_DATA_SIZE     512
 
 static uint8_t m_data[TRACE_DATA_SIZE] = {0};
 static int m_cbuf_index = -1;
 
-void usart1_isr(void)
+void usart2_isr(void)
 {
     // Check if we were called because of txe.
-    if (((USART_CR1(USART1) & USART_CR1_TXEIE) != 0)
-        && ((USART_SR(USART1) & USART_SR_TXE) != 0)) {
+    if (((USART_CR1(USART2) & USART_CR1_TXEIE) != 0)
+        && ((USART_SR(USART2) & USART_SR_TXE) != 0)) {
 
-        usart_disable_tx_interrupt(USART1);
+        usart_disable_tx_interrupt(USART2);
         uint8_t c = 0;
         if (fwapp_cbuf_get(m_cbuf_index, &c)) {
-            usart_send(USART1, c);
-            usart_enable_tx_interrupt(USART1);
+            usart_send(USART2, c);
+            usart_enable_tx_interrupt(USART2);
         }
     }
 }
@@ -31,7 +33,7 @@ void usart1_isr(void)
 extern int _write(int file, char *ptr, int len);
 int _write(int file, char *ptr, int len)
 {
-    usart_disable_tx_interrupt(USART1);
+    usart_disable_tx_interrupt(USART2);
 
     int rc = -1;
     if (file == 1) {
@@ -47,7 +49,7 @@ int _write(int file, char *ptr, int len)
         errno = EIO;
     }
 
-    usart_enable_tx_interrupt(USART1);
+    usart_enable_tx_interrupt(USART2);
     return rc;
 }
 
@@ -56,24 +58,24 @@ void fwapp_trace_start(uint32_t baud_rate)
     m_cbuf_index = fwapp_cbuf_open(m_data, TRACE_DATA_SIZE);
 
     // Enable usart interrupts.
-    nvic_enable_irq(NVIC_USART1_IRQ);
+    nvic_enable_irq(NVIC_USART2_IRQ);
     // Configure usart tx pin.
     gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
-                  GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART1_TX);
+                  GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART2_TX);
     // Setup usrt parameters.
-    usart_set_baudrate(USART1, baud_rate);
-    usart_set_databits(USART1, 8);
-    usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
-    usart_set_mode(USART1, USART_MODE_TX);
-    usart_set_parity(USART1, USART_PARITY_NONE);
-    usart_set_stopbits(USART1, USART_STOPBITS_1);
+    usart_set_baudrate(USART2, baud_rate);
+    usart_set_databits(USART2, 8);
+    usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+    usart_set_mode(USART2, USART_MODE_TX);
+    usart_set_parity(USART2, USART_PARITY_NONE);
+    usart_set_stopbits(USART2, USART_STOPBITS_1);
     // Enable usart.
-    usart_enable(USART1);
+    usart_enable(USART2);
 }
 
 void fwapp_trace_stop(void)
 {
-    usart_disable(USART1);
-    nvic_disable_irq(NVIC_USART1_IRQ);
+    usart_disable(USART2);
+    nvic_disable_irq(NVIC_USART2_IRQ);
     fwapp_cbuf_close(m_cbuf_index);
 }
